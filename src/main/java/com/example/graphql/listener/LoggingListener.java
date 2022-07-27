a5aab631f2e76c0c56db3f34a2cbd143ab19dc85
Karolina Pacheco <karolina_cris@hotmail.com>
2022-07-27 17:27:35 -0300

    add custom listener

---
+++ src/main/java/com/example/graphql/listener/LoggingListener.java
@@ -0,0 +1,45 @@
+package com.example.graphql.listener;
+
+import graphql.kickstart.servlet.core.GraphQLServletListener;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class LoggingListener implements GraphQLServletListener {
+
+    private final Clock clock;
+
+    @Override
+    public RequestCallback onRequest(HttpServletRequest request, HttpServletResponse response) {
+        Instant startTime = Instant.now(clock);
+        log.info("GraphQL request received.");
+        return new RequestCallback() {
+            @Override
+            public void onSuccess(HttpServletRequest request, HttpServletResponse response) {
+                // no-op
+            }
+
+            @Override
+            public void onError(HttpServletRequest request, HttpServletResponse response, Throwable throwable) {
+                // no-op
+            }
+
+            @Override
+            public void onFinally(HttpServletRequest request, HttpServletResponse response) {
+                // The final callback in the GraphQL life-cycle
+                log.info("Completed Request. Time taken: {}", Duration.between(startTime, Instant.now(clock)));
+            }
+        };
+    }
+
+}
