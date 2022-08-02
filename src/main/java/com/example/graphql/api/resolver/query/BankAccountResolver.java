91776fb3beef2d6e64d50628d048000e19c58fdc
Karolina Pacheco <karolina_cris@hotmail.com>
2022-08-02 10:03:25 -0300

    Add custom context

--- src/main/java/com/example/graphql/api/resolver/query/BankAccountResolver.java
+++ src/main/java/com/example/graphql/api/resolver/query/BankAccountResolver.java
@@ -1,7 +1,9 @@
 package com.example.graphql.api.resolver.query;
 
+import com.example.graphql.context.CustomGraphQLContext;
 import com.example.graphql.domain.bank.BankAccount;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import graphql.schema.DataFetchingEnvironment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +15,10 @@ import static com.example.graphql.domain.bank.Currency.USD;
 @Slf4j
 public class BankAccountResolver implements GraphQLQueryResolver {
 
-    public BankAccount getBankAccount(UUID id) {
+    public BankAccount getBankAccount(UUID id, DataFetchingEnvironment environment) {
+        CustomGraphQLContext context = environment.getContext();
+        log.info("User id from context: {}", context.getUserId());
+
         log.info("Retrieving bank account with id: {}", id);
         return BankAccount.builder()
                 .id(id)
---
+++ src/main/java/com/example/graphql/context/CustomGraphQLContextBuilder.java
@@ -0,0 +1,41 @@
+package com.example.graphql.context;
+
+import graphql.kickstart.execution.context.DefaultGraphQLContext;
+import graphql.kickstart.execution.context.GraphQLContext;
+import graphql.kickstart.servlet.context.DefaultGraphQLServletContext;
+import graphql.kickstart.servlet.context.GraphQLServletContext;
+import graphql.kickstart.servlet.context.GraphQLServletContextBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.websocket.Session;
+import javax.websocket.server.HandshakeRequest;
+
+@Slf4j
+@Component
+public class CustomGraphQLContextBuilder implements GraphQLServletContextBuilder {
+
+    @Override
+    public GraphQLContext build(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        String userId = httpServletRequest.getHeader("user_id");
+
+        GraphQLServletContext context = DefaultGraphQLServletContext.createServletContext()
+                .with(httpServletRequest)
+                .with(httpServletResponse)
+                .build();
+
+        return new CustomGraphQLContext(userId, context);
+    }
+
+    @Override
+    public GraphQLContext build(Session session, HandshakeRequest handshakeRequest) {
+        throw new IllegalStateException("Unsupported");
+    }
+
+    @Override
+    public GraphQLContext build() {
+        throw new IllegalStateException("Unsupported");
+    }
+}
