76021075bf981aab8a90f80048e323a8e6f22e37
Karolina Pacheco <karolina_cris@hotmail.com>
2022-09-07 18:37:00 -0300

    Fix contextLoads test

--- src/test/java/com/example/graphql/GraphqlApplicationTests.java
+++ src/test/java/com/example/graphql/GraphqlApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.graphql;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+@SpringBootTest(webEnvironment= RANDOM_PORT)
 class GraphqlApplicationTests {
 
 	@Test
